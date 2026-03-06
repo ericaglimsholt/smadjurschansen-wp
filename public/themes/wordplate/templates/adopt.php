a7613903267b9fab00b4b6ae55bf2c642375efaf
Erica  Glimsholt <ericaglimsholt@gmail.com>
2026-03-06 10:14:40 +0100

    Add adopt.css and adjust adopt template layout
    Create public/themes/wordplate/templates/adopt.css and include it in adopt.php. Update adopt.php markup to wrap the header content in a column div (column--12 / tablet / mobile) so the template can use the new stylesheet and has proper responsive structure.

--- public/themes/wordplate/templates/adopt.php
+++ public/themes/wordplate/templates/adopt.php
@@ -6,13 +6,16 @@ Template Name: Intresseanmälan
 
 <style>
 <?php include 'base.css'; ?>
+<?php include 'adopt.css'; ?>
 </style>
 
 <?php get_header(); ?>
 
 <div class="adopt">
   <div class="row header no-description">
-    <h1>Lämna intresseanmälan</h1>
-    <?= the_content(); ?>
+    <div class="column column--12 column__tablet--12 column__mobile--12">
+      <h1>Lämna intresseanmälan</h1>
+      <?= the_content(); ?>
+    </div>
   </div>
 </div>
